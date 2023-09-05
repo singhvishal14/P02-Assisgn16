06c5b3f0fbdde27043432eb737c7bbd79013c3df
singhvishal14 <kumarvishalsingh767@gmail.com>
2023-09-05 23:25:42 +0530

    first commit

---
+++ WebAppAssisgn16/Data/WebAppAssisgn16DbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAppAssisgn16.Models;
+
+namespace WebAppAssisgn16.Data
+{
+    public class WebAppAssisgn16DbContext : DbContext
+    {
+        public WebAppAssisgn16DbContext (DbContextOptions<WebAppAssisgn16DbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAppAssisgn16.Models.Task> Task { get; set; } = default!;
+    }
+}
---
+++ WebAppAssisgn16/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebAppAssisgn16.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<WebAppAssisgn16DbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("WebAppAssisgn16DbContext") ?? throw new InvalidOperationException("Connection string 'WebAppAssisgn16DbContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
